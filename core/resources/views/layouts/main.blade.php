76da7dfde65811148339ff186cc34199e2988e2d
lincontroy <lincolnmunene37@gmail.com>
2024-12-20 00:55:07 +0300

    changed to a color

--- core/resources/views/layouts/main.blade.php
+++ core/resources/views/layouts/main.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en" dir="ltr">
+<html lang="en" dir='ltr'>
 
 <head>
     <meta charset="UTF-8">
@@ -25,7 +25,7 @@
 
 </head>
 
-<body class="layout-dark side-menu overlayScroll">
+<body class="side-menu overlayScroll">
     <div class="mobile-search">
         <form class="search-form">
             <span data-feather="search"></span>
@@ -35,12 +35,12 @@
 
     <div class="mobile-author-actions"></div>
     <header class="header-top">
-        <nav class="navbar navbar-dark">
+        <nav class="navbar">
             <div class="navbar-left">
                 <a href="#" class="sidebar-toggle">
                     <img class="svg" src="{{ url('img/svg/bars.svg') }}" alt="img"></a>
                 <a class="navbar-brand" href="#">
-                    <h2 style="color:white">Ebbaymart</h2>
+                    <h2>Ebbaymart</h2>
 
                   
                 </a>
@@ -650,79 +650,9 @@
             </div>
         </span>
     </div>
-    <div class="overlay-dark-sidebar"></div>
-    <div class="customizer-overlay"></div>
+    
+    
 
-    <a href="#" class="customizer-trigger">
-        <span data-feather="settings"></span></a>
-    <div class="customizer-wrapper">
-        <div class="customizer">
-            <div class="customizer__head">
-                <h4 class="customizer__title">Customizer</h4>
-                <span class="customizer__sub-title">Customize your overview page layout</span>
-                <a href="#" class="customizer-close">
-                    <span data-feather="x"></span></a>
-            </div>
-            <div class="customizer__body">
-                <div class="customizer__single">
-                    <h4>Layout Type</h4>
-                    <ul class="customizer-list d-flex layout">
-                        <li class="customizer-list__item">
-                            <a href="https://demo.jsnorm.com/html/strikingdash/strikingdash/ltr" class="active">
-                                <img src="img/ltr.png" alt="">
-                                <i class="fa fa-check-circle"></i>
-                            </a>
-                        </li>
-                        <li class="customizer-list__item">
-                            <a href="https://demo.jsnorm.com/html/strikingdash/strikingdash/rtl">
-                                <img src="img/rtl.png" alt="">
-                                <i class="fa fa-check-circle"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- ends: .customizer__single -->
-
-                <div class="customizer__single">
-                    <h4>Sidebar Type</h4>
-                    <ul class="customizer-list d-flex l_sidebar">
-                        <li class="customizer-list__item">
-                            <a href="#" data-layout="light" class="active">
-                                <img src="img/light.png" alt="">
-                                <i class="fa fa-check-circle"></i>
-                            </a>
-                        </li>
-                        <li class="customizer-list__item">
-                            <a href="#" data-layout="dark">
-                                <img src="img/dark.png" alt="">
-                                <i class="fa fa-check-circle"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- ends: .customizer__single -->
-
-                <div class="customizer__single">
-                    <h4>Navbar Type</h4>
-                    <ul class="customizer-list d-flex l_navbar">
-                        <li class="customizer-list__item">
-                            <a href="#" data-layout="side" class="active">
-                                <img src="img/side.png" alt="">
-                                <i class="fa fa-check-circle"></i>
-                            </a>
-                        </li>
-                        <li class="customizer-list__item top">
-                            <a href="#" data-layout="top">
-                                <img src="img/top.png" alt="">
-                                <i class="fa fa-check-circle"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- ends: .customizer__single -->
-            </div>
-        </div>
-    </div>
 
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
